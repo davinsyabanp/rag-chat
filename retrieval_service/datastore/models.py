8959e886569992313c1f404a265bef8c4ec2856e
Davin Sya’ban <dsyabanp@gmail.com>
2025-05-20 02:55:36 +0700

    Refactor Service, Kursus, and Faq classes to inherit from BaseModel for improved data validation

--- retrieval_service/datastore/models.py
+++ retrieval_service/datastore/models.py
@@ -1,57 +1,30 @@
 from typing import List, Optional
+from pydantic import BaseModel
 
 
-class Service:
-    def __init__(
-        self,
-        id: int,
-        category: str,
-        title: str,
-        description: str,
-        price: int,
-        embedding: Optional[List[float]] = None,
-    ):
-        self.id = id
-        self.category = category
-        self.title = title
-        self.description = description
-        self.price = price
-        self.embedding = embedding
+class Service(BaseModel):
+    id: int
+    category: str
+    title: str
+    description: str
+    price: int
+    embedding: Optional[List[float]] = None
 
 
-class Kursus:
-    def __init__(
-        self,
-        id: int,
-        course_name: str,
-        level: str,
-        description: str,
-        price: int,
-        start_date: str,
-        end_date: str,
-        embedding: Optional[List[float]] = None,
-    ):
-        self.id = id
-        self.course_name = course_name
-        self.level = level
-        self.description = description
-        self.price = price
-        self.start_date = start_date
-        self.end_date = end_date
-        self.embedding = embedding
+class Kursus(BaseModel):
+    id: int
+    course_name: str
+    level: str
+    description: str
+    price: int
+    start_date: str
+    end_date: str
+    embedding: Optional[List[float]] = None
 
 
-class Faq:
-    def __init__(
-        self,
-        id: int,
-        category: str,
-        title: str,
-        description: str,
-        embedding: Optional[List[float]] = None,
-    ):
-        self.id = id
-        self.category = category
-        self.title = title
-        self.description = description
-        self.embedding = embedding
+class Faq(BaseModel):
+    id: int
+    category: str
+    title: str
+    description: str
+    embedding: Optional[List[float]] = None
